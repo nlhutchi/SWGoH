4132212efaaa494b1afd385dbfb4ee718897b4c0
nlhutchi <30396281+nlhutchi@users.noreply.github.com>
2021-04-19 23:51:16 -0700

    Trigger Workflow Final?

--- UIProject/src/App.js
+++ UIProject/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 4
+          Learn React final
         </a>
       </header>
     </div>
