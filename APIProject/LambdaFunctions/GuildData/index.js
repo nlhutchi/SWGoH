42fa5023714b8fa4ffbcdf727f112fb41ff5e830
nlhutchi <30396281+nlhutchi@users.noreply.github.com>
2022-01-15 22:43:22 -0800

    Feature/guild data (#1)
    * Create index.js
    
    * Create package.json
    
    * Create package.json
    
    * Delete package.json
    
    * Added Layer
    
    * Update endpointMapping.json
    
    * Create endpointMapping.json
    
    * Update index.js
    
    * Added Guild Data Function

---
+++ APIProject/LambdaFunctions/GuildData/index.js
@@ -0,0 +1,70 @@
+const AWS = require("aws-sdk");
+const axios = require('axios');
+const endpointMapping = require("./endpointMapping.json");
+var axiosInstance;
+
+var returnObj = {
+    statusCode: null,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    body: null,
+};
+
+
+
+exports.handler = async (event, context, callback) => {
+    console.log('Event', event);
+    if(!axiosInstance) {
+        axiosInstance = createAxiosInstance(event.headers.Authorization);
+    }
+    var body = event.body;
+
+    try {
+        switch (event.httpMethod) {
+            case "GET": 
+                switch (event.path) {
+                    case endpointMapping.GET.GuildData.path:
+                        console.log("Endpoint: ", endpointMapping.GET.GuildData.description);
+                        await getGuildData(body, axiosInstance);
+                        return callback(null, returnObj);
+                    default:
+                        returnObj.body = "Path not found";
+                        returnObj.statusCode = 404;
+                        return callback(null, returnObj);
+                }
+            default:
+                returnObj.body = "Method Not Allowed";
+                returnObj.statusCode = 405;
+                return callback(null, returnObj);
+        }
+
+    } catch (err) {
+        console.log("ERROR: ", err);
+        returnObj.statusCode = 500;
+        returnObj.body = JSON.stringify(err);
+        return callback(null, returnObj);
+    }
+};
+
+async function getGuildData() {
+    await axiosInstance.post('https://api.swgoh.help/swgoh/guilds', { allyCodes: body.allyCodes })
+        .then((data) => {
+            console.log("Success", data);
+            returnObj.body = JSON.stringify({ ...data.data });
+            returnObj.statusCode = 200;
+        })
+        .catch((err) => {
+            console.log("Error", err);
+            returnObj.body = JSON.stringify({ message: 'failed', details: err });
+            returnObj.statusCode = 400;
+        })
+}
+
+function createAxiosInstance(auth) {
+    return axios.create({
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: auth,
+        },
+    });
+}
