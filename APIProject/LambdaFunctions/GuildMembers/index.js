e0c8e5c42e3341b1d2baab8e8f97b77ae833bd8e
nathanhutchinson <30396281+nlhutchi@users.noreply.github.com>
2024-02-01 15:47:28 -0800

    Event for updating guild members

---
+++ APIProject/LambdaFunctions/GuildMembers/index.js
@@ -0,0 +1,74 @@
+const axios = require('axios');
+var documentClient = new AWS.DynamoDB.DocumentClient();
+
+var axiosInstance;
+
+const createAxiosInstance = () => {
+    return axios.create({
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+    });
+}
+
+exports.handler = async (event, context, callback) => {
+    if(!axiosInstance) {
+        axiosInstance = createAxiosInstance();
+    }
+
+    var guildMembers = await getGuildMembers();
+    console.log('guildMembers', guildMembers)
+    //await insertMembersToDB(process.env.GuildMemberTable, guildMembers);
+
+    return callback(null, returnObj);
+                
+}
+
+async function getGuildMembers(guildId) {
+    console.log('getGuildDataGG')
+    var guildMembers;
+
+    await axiosInstance.get(`http://api.swgoh.gg/guild-profile/${guildId}/`)
+        .then((data) => {
+            console.log("Success", data);
+            guildMembers = data.data;
+        })
+        .catch((err) => {
+            console.log("Error", err);
+        });
+    
+    return guildMembers;
+}
+
+
+const insertMembersToDB = async (guildMemberTable, guildMembers) => {
+    console.log('guildMembers', guildMembers)
+    var tableArray = guildMembers.map((member) => {
+        return {
+            PutRequest: {
+              Item: {
+                allyCode: member.allyCode,
+                name: member.name,
+                gp: member.GP
+              }
+            }
+          }
+    })
+
+    console.log("tableArray", tableArray);
+
+    var params = {
+        RequestItems: {
+          [guildMemberTable]: tableArray
+        }
+      };
+      
+      await documentClient.batchWrite(params).promise()
+        .then((response) => {
+            console.log('response', response);
+        }).catch((err) => {
+            console.log('err', err);
+        })
+}
